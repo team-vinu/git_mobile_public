3db38bf7a521ccd664cd8fdd0178c3c048d04922
Satsuki Akiba <anosatsuk124@gmail.com>
2022-07-08 04:03:47 +0000

    update

--- build_ios.sh
+++ build_ios.sh
@@ -2,15 +2,14 @@
 
 DIR="./openssl-1.1.1p"
 
-if [ ! -d $DIR ]; then
-    wget https://www.openssl.org/source/openssl-1.1.1p.tar.gz
-    tar xvf "${DIR}.tar.gz"
-    rm "${DIR}.tar.gz"
-fi
-
 function aarch64() {
     OUTPUT_DIR="$(pwd)/${DIR}/aarch64_ios"
 
+    if [ ! -d $DIR ]; then
+        wget https://www.openssl.org/source/openssl-1.1.1p.tar.gz
+        tar xvf "${DIR}.tar.gz"
+        rm "${DIR}.tar.gz"
+    fi
     rustup target add aarch64-apple-ios aarch64-apple-ios-sim
 
     if [ ! -d ${OUTPUT_DIR} ]; then
@@ -43,8 +42,36 @@ function aarch64() {
     # cargo build --release --target=aarch64-apple-ios-sim 
 }
 
+function aarch64_sim(){
+    OUTPUT_DIR="$(pwd)/${DIR}/aarch64_iossim"
+
+    if [ ! -d $DIR ]; then
+        wget https://www.openssl.org/source/openssl-1.1.1p.tar.gz
+        tar xvf "${DIR}.tar.gz"
+        rm "${DIR}.tar.gz"
+    fi
+
+    if [ ! -d ${OUTPUT_DIR} ]; then
+        cd "${DIR}"
+
+        ./Configure iossimulator-xcrun --prefix=${OUTPUT_DIR}
+
+        CROSS_COMPILE=`xcode-select --print-path`/Toolchains/XcodeDefault.xctoolchain/usr/bin/ CROSS_TOP=`xcode-select --print-path`/Platforms/iPhoneOS.platform/Developer CROSS_SDK=iPhoneOS.sdk make
+
+        make install_sw
+
+        cd .. 
+    else
+        :
+    fi
+
+    mv "${OUTPUT_DIR}" ./openssl
+    rm -r "${DIR}"
+}
+
 if [ "$1" = "all" ]; then
     aarch64
+    aarch64_sim
 else
     :
 fi
