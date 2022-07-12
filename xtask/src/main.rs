4ff65d68c11146019357ba45a5c2854772c509e7
Satsuki Akiba <anosatsuk124@gmail.com>
2022-07-12 13:46:06 +0900

    support for windows

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -108,8 +108,16 @@ fn flutter_run(device: Option<&String>) {
         I686_LINUX_ANDROID_OPENSSL_LIB_DIR,
     );
 
-    let mut fvm = Command::new("fvm");
-    let mut flutter = Command::new("flutter");
+    let mut fvm = if cfg!(windows) {
+        Command::new("fvm.bat")
+    } else {
+        Command::new("fvm")
+    };
+    let mut flutter = if cfg!(windows) {
+        Command::new("flutter.bat")
+    } else {
+        Command::new("flutter")
+    };
     let args = match device {
         Some(dev) => vec!["run".to_string(), "-d".to_string(), format!("{}", &dev)],
         None => vec!["run".to_string()],
