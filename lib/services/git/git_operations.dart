8988d229dafe6b91e2e912a03f48772c666fa005
Satsuki Akiba <anosatsuk124@gmail.com>
2022-07-13 21:08:51 +0000

    change error class definitions

--- lib/services/git/git_operations.dart
+++ lib/services/git/git_operations.dart
@@ -6,30 +6,31 @@ import 'package:git_mobile/ffi.dart';
 import 'package:git_mobile/utility/enums/error.dart';
 import 'package:git_mobile/utility/types/fp_alias.dart';
 
-Future<FpResult<FpUnit>> gitInit(String path) async {
-  final result = await git.gitInit(dir: path);
-  return result.match((err) {
-    return FpResult<FpUnit>.left(Error.gitError(err, fp.none()));
-  }, (_) {
-    return FpResult<FpUnit>.of(fp.unit);
-  });
-}
+fp.Task<FpResult<fp.Unit>> gitInit(String path) => fp.Task(() async {
+      final result = await git.gitInit(dir: path);
+      return result.match((err) {
+        return FpResult<fp.Unit>.left(Error.gitError(err));
+      }, (_) {
+        return FpResult<fp.Unit>.of(fp.unit);
+      });
+    });
 
-Future<FpResult<FpUnit>> gitOpen(String path) async {
-  final result = await git.gitOpen(dir: path);
-  return result.match(
-    (err) => FpResult<FpUnit>.left(Error.gitError(err, fp.none())),
-    (_) => FpResult<FpUnit>.of(fp.unit),
-  );
-}
+fp.Task<FpResult<fp.Unit>> gitOpen(String path) => fp.Task(() async {
+      final result = await git.gitOpen(dir: path);
+      return result.match(
+        (err) => FpResult<fp.Unit>.left(Error.gitError(err)),
+        (_) => FpResult<fp.Unit>.of(fp.unit),
+      );
+    });
 
-Future<FpResult<FpUnit>> gitClone(String path, String url) async {
-  final result = await (Platform.isAndroid
-      ? git.gitHttpClone(dir: path, url: url)
-      : git.gitHttpsClone(dir: path, url: url));
-  return result.match((err) {
-    return FpResult<FpUnit>.left(Error.gitError(err, fp.none()));
-  }, (_) {
-    return FpResult<FpUnit>.of(fp.unit);
-  });
-}
+fp.Task<FpResult<fp.Unit>> gitClone(String path, String url) => fp.Task(
+      () async {
+        final result = await (Platform.isAndroid
+            ? git.gitHttpClone(dir: path, url: url)
+            : git.gitHttpsClone(dir: path, url: url));
+        return result.match(
+          (err) => FpResult<fp.Unit>.left(Error.gitError(err)),
+          (ok) => FpResult<fp.Unit>.of(fp.unit),
+        );
+      },
+    );
