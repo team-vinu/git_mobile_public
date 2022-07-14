e0a9cd423d8054d23e417c7e3291cf557b0d3e9e
wadeyusaku <wadeyusaku@gmail.com>
2022-07-14 14:43:54 +0900

    fix import

--- lib/ui/screen/routes/app_route.dart
+++ lib/ui/screen/routes/app_route.dart
@@ -1,6 +1,6 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:git_mobile/screen/routes/route_path.dart';
-import 'package:git_mobile/screen/home/home_screen.dart';
+import 'package:git_mobile/ui/screen/routes/route_path.dart';
+import 'package:git_mobile/ui/screen/home/home_screen.dart';
 
 @AdaptiveAutoRouter(
   replaceInRouteName: 'Screen,Route',
